17b55ba6d23fcfefc7c2bf9c568ae13c30d87be6
Chad Lengefeld <chadlengefeld@gmail.com>
2020-04-03 09:34:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -21,7 +21,7 @@ class App < Sinatra::Base
     @goodbye = all_the_names.select do |name|
       name.id == params[:name]
     end.first
-    erb :'./medicines.show.html'
+    erb :'./names.show.html'
   end
 
 end
